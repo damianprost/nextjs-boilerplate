9127fc83ce73618d80c1157656e46ec6cd17f81f
damianprost <damianprost2025@gmail.com>
2026-07-12 22:36:59 -0300

    Update GoogleLoginButton.tsx

--- app/components/GoogleLoginButton.tsx
+++ app/components/GoogleLoginButton.tsx
@@ -10,6 +10,9 @@ export default function GoogleLoginButton() {
       provider: 'google',
       options: {
         redirectTo: `${window.location.origin}/auth/callback`,
+        queryParams: {
+          prompt: 'select_account',
+        },
       },
     })
   }
